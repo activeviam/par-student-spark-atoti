8b92852087e7880523dbdb4fed4352949ec28f98
Mathis Pierron <mathis.pierron@student-cs.fr>
2021-11-04 19:42:03 +0100

    First draft of discovery

--- src/main/java/io/atoti/spark/Discovery.java
+++ src/main/java/io/atoti/spark/Discovery.java
@@ -1,11 +1,14 @@
 package io.atoti.spark;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
 public class Discovery {
 
 	private Discovery() {}
 
-	public static Object discoverDataframe(Object dataframe) {
-		throw new UnsupportedOperationException("TODO");
+	public static void discoverDataframe(Dataset<Row> dataframe) {
+		dataframe.printSchema();
+		dataframe.show();
 	}
-
 }
--- src/test/java/io/atoti/spark/TestDiscovery.java
+++ src/test/java/io/atoti/spark/TestDiscovery.java
@@ -3,28 +3,41 @@ package io.atoti.spark;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.net.URISyntaxException;
-import java.nio.file.Path;
+import java.net.URL;
 import java.util.Objects;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
 import org.junit.jupiter.api.Test;
 
+import scala.Tuple2;
+
 public class TestDiscovery {
+	
+	SparkSession spark = SparkSession.builder().appName("Spark Atoti").config("spark.master", "local").getOrCreate();
 
   @Test
   void testDiscovery() throws URISyntaxException {
     final ClassLoader cl = Thread.currentThread().getContextClassLoader();
-    final var url = Objects.requireNonNull(cl.getResource("csv/basic.csv"), "Cannot find file");
-    final var path = Path.of(url.toURI());
+    final URL url = Objects.requireNonNull(cl.getResource("csv/basic.csv"), "Cannot find file");
+    
+    System.out.println(url);
 
-    if (true) {
-      throw new UnsupportedOperationException("Load the path as as dataframe and ");
-    }
-    final Object dataframe = null; // convert path to dataframe
+    System.out.println(url.toURI().getPath());
+	final Dataset<Row> dataframe = spark.read()
+			.format("csv")
+			.option("sep", ";")
+			.option("header", true)
+			.option("dateFormat", "dd/MM/yyyy")
+            .option("inferSchema", true)
+            .load(url.toURI().getPath());
     Discovery.discoverDataframe(dataframe);
 
     assertThat(dataframe).isNotNull();
-    // TODO: add more assertions about the discovered values in the dataframe
-    // we should see 3 columns: id, label and value
-    // we should see that id is a number, label is something string-y and value is a number, ideally
-    // floating.
+    assertThat(dataframe.dtypes()).contains(new Tuple2<String, String>("id", DataTypes.IntegerType.toString()));
+    assertThat(dataframe.dtypes()).contains(new Tuple2<String, String>("label", DataTypes.StringType.toString()));
+    assertThat(dataframe.dtypes()).contains(new Tuple2<String, String>("value", DataTypes.DoubleType.toString()));
   }
 }
