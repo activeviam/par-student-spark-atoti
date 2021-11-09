281957dc368eb37229b3aa9c41d4bd8f369eb55b
arnaudframmery <arnaud.frammery@gmail.com>
2021-11-09 11:19:05 +0100

    add a test for a dataframe with some computed columns

--- src/test/java/io/atoti/spark/TestDiscovery.java
+++ src/test/java/io/atoti/spark/TestDiscovery.java
@@ -26,6 +26,7 @@ public class TestDiscovery {
     final URL url = Objects.requireNonNull(cl.getResource("csv/basic.csv"), "Cannot find file");
     final URL urlTwoTypesInSameColumn = Objects.requireNonNull(cl.getResource("csv/twoTypesInSameColumn.csv"), "Cannot find file");
     final URL urlToJoin = Objects.requireNonNull(cl.getResource("csv/toJoin.csv"), "Cannot find file");
+    final URL urlCalculate = Objects.requireNonNull(cl.getResource("csv/calculate.csv"), "Cannot find file");
 
     System.out.println(url.toURI().getPath());
 	final Dataset<Row> dataframe = spark.read()
@@ -86,5 +87,28 @@ public class TestDiscovery {
     assertThat(dataframeJoin.dtypes()).contains(new Tuple2<>("join_id", DataTypes.IntegerType.toString()));
     assertThat(dataframeJoin.dtypes()).contains(new Tuple2<>("join_value", DataTypes.DoubleType.toString()));
     assertThat(dataframeJoin.dtypes()).contains(new Tuple2<>("join_date", DataTypes.TimestampType.toString()));
+
+    System.out.println(urlCalculate);
+    System.out.println(urlCalculate.toURI().getPath());
+
+    Dataset<Row> dataframeCalculate = spark.read()
+          .format("csv")
+          .option("sep", ";")
+          .option("header", true)
+          .option("timestampFormat", "dd/MM/yyyy")
+          .option("inferSchema", true)
+          .load(urlCalculate.toURI().getPath());
+    dataframeCalculate = dataframeCalculate
+          .withColumn("val1_minus_val2",dataframeCalculate.col("val1").minus(dataframeCalculate.col("val2")))
+          .withColumn("val1_equal_val2",dataframeCalculate.col("val1").equalTo(dataframeCalculate.col("val2")));
+
+    Discovery.discoverDataframe(dataframeCalculate);
+
+    assertThat(dataframeCalculate).isNotNull();
+    assertThat(dataframeCalculate.dtypes()).contains(new Tuple2<>("id", DataTypes.IntegerType.toString()));
+    assertThat(dataframeCalculate.dtypes()).contains(new Tuple2<>("val1", DataTypes.IntegerType.toString()));
+    assertThat(dataframeCalculate.dtypes()).contains(new Tuple2<>("val2", DataTypes.DoubleType.toString()));
+    assertThat(dataframeCalculate.dtypes()).contains(new Tuple2<>("val1_minus_val2", DataTypes.DoubleType.toString()));
+    assertThat(dataframeCalculate.dtypes()).contains(new Tuple2<>("val1_equal_val2", DataTypes.BooleanType.toString()));
   }
 }
