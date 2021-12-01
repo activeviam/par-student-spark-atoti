9814b22f5716fb19c39b660fe617ec7e6d6eaf64
arnaudframmery <arnaud.frammery@gmail.com>
2021-12-01 10:13:19 +0100

    return a mapping of columns/dtypes + tests adaptation

--- src/test/java/io/atoti/spark/TestDiscovery.java
+++ src/test/java/io/atoti/spark/TestDiscovery.java
@@ -1,9 +1,11 @@
 package io.atoti.spark;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Map;
 import java.util.Objects;
 
 import org.apache.spark.sql.Dataset;
@@ -12,7 +14,6 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataTypes;
 import org.junit.jupiter.api.Test;
 
-import scala.Tuple2;
 
 public class TestDiscovery {
   SparkSession spark =
@@ -31,11 +32,16 @@ public class TestDiscovery {
 			.option("dateFormat", "dd/MM/yyyy")
             .option("inferSchema", true)
             .load(url.toURI().getPath());
-    Discovery.discoverDataframe(dataframe);
+    final Map<String, String> dTypes = Discovery.discoverDataframe(dataframe);
 
     assertThat(dataframe).isNotNull();
-    assertThat(dataframe.dtypes()).contains(new Tuple2<String, String>("id", DataTypes.IntegerType.toString()));
-    assertThat(dataframe.dtypes()).contains(new Tuple2<String, String>("label", DataTypes.StringType.toString()));
-    assertThat(dataframe.dtypes()).contains(new Tuple2<String, String>("value", DataTypes.DoubleType.toString()));
+    assertThat(dTypes).isNotNull();
+
+    assertTrue(dTypes.containsKey("id"));
+    assertThat(dTypes.get("id")).isEqualTo(DataTypes.IntegerType.toString());
+    assertTrue(dTypes.containsKey("label"));
+    assertThat(dTypes.get("label")).isEqualTo(DataTypes.StringType.toString());
+    assertTrue(dTypes.containsKey("value"));
+    assertThat(dTypes.get("value")).isEqualTo(DataTypes.DoubleType.toString());
   }
 }
