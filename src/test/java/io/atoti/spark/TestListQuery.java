9b72aa225c922265e2382633325433acde9cb0a5
arnaudframmery <arnaud.frammery@gmail.com>
2021-12-02 14:32:05 +0100

    add test when limit parameter is bigger than dataframe size

--- src/test/java/io/atoti/spark/TestListQuery.java
+++ src/test/java/io/atoti/spark/TestListQuery.java
@@ -77,6 +77,13 @@ public class TestListQuery {
   }
 
   @Test
+  void testTooBigLimit() throws URISyntaxException {
+    final Dataset<Row> dataframe = CsvReader.read("csv/basic.csv", spark);
+    final var rows = ListQuery.list(dataframe, List.of("id", "value"), 5, 0);
+    assertThat(rows).hasSize(3);
+  }
+
+  @Test
   void testListWithCondition() throws URISyntaxException {
     final Dataset<Row> dataframe = CsvReader.read("csv/basic.csv", spark);
     final var rows = ListQuery.list(dataframe, new EqualCondition("id", 3));
