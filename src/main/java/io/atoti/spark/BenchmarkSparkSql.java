e4e0994127b21dba2042944e0f6e106a3a005982
arnaudframmery <arnaud.frammery@gmail.com>
2021-12-15 11:02:29 +0100

    add the first benchmark on Spark sql

--- src/main/java/io/atoti/spark/BenchmarkSparkSql.java
+++ src/main/java/io/atoti/spark/BenchmarkSparkSql.java
@@ -7,22 +7,22 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @State(Scope.Benchmark)
 @Fork(value=1, jvmArgs={"--enable-preview", "--illegal-access=permit"})
-public class BenchmarkListQuery {
+public class BenchmarkSparkSql {
     SparkSession spark;
     Dataset<Row> dataframe;
+    String tableName;
     int limit;
     int offset;
     List<String> wantedColumns;
-    
+
     public static void main(String[] args) throws Exception {
         Options opt = new OptionsBuilder()
-                .include(BenchmarkListQuery.class.getSimpleName())
+                .include(BenchmarkSparkSql.class.getSimpleName())
                 .build();
         new Runner(opt).run();
     }
@@ -32,9 +32,11 @@ public class BenchmarkListQuery {
         spark = SparkSession.builder().appName("Spark Atoti").config("spark.master", "local").getOrCreate();
         spark.sparkContext().setLogLevel("ERROR");
         dataframe = CsvReader.read("csv/US_accidents_Dec20_updated.csv", spark, ",");
+        tableName = "my_table";
         limit = 100000;
         offset = 100000;
         wantedColumns = List.of("ID", "Severity");
+        dataframe.createOrReplaceTempView(tableName);
     }
 
     @Benchmark
@@ -42,7 +44,7 @@ public class BenchmarkListQuery {
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
     @Warmup(iterations = 3)
     @Measurement(iterations = 10)
-    public void BenchmarkColumnId(Blackhole bh) {
+    public void benchmarkSparkApi(Blackhole bh) {
         final List<Row> rows = ListQuery.list(dataframe, wantedColumns, limit, offset);
         bh.consume(rows);
     }
@@ -52,24 +54,9 @@ public class BenchmarkListQuery {
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
     @Warmup(iterations = 3)
     @Measurement(iterations = 10)
-    public void BenchmarkLimitAndTail(Blackhole bh) {
-        List<Row> rows;
-
-        if (offset < 0) {
-            throw new IllegalArgumentException("Cannot accept a negative offset");
-        }
-
-        final Column[] columns = wantedColumns.stream().map(functions::col).toArray(Column[]::new);
-        final int dfSize = (int) dataframe.count();
-        bh.consume(dfSize);
-
-        if(limit >= 0) {
-            rows = Arrays.asList((Row[]) dataframe.select(columns).limit(limit + offset).tail(
-                    limit + offset <= dfSize ? limit : Math.max(0, dfSize - offset)
-            ));
-        } else {
-            rows = Arrays.asList((Row[]) dataframe.select(columns).tail(Math.max(dfSize - offset, 0)));
-        }
+    public void benchmarkSparkSql(Blackhole bh) {
+        final List<Row> rows = ListQuery.listSql(spark, tableName, wantedColumns, limit, offset);
         bh.consume(rows);
     }
 }
+
