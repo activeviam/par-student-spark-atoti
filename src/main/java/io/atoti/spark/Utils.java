d4c50f9664e59a2f127e401ebbccd3c8484a81bb
Mathis Pierron <mathis.pierron@student-cs.fr>
2022-03-23 11:27:19 +0100

    Tackle review comments

--- src/main/java/io/atoti/spark/Utils.java
+++ src/main/java/io/atoti/spark/Utils.java
@@ -1,28 +1,25 @@
 package io.atoti.spark;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import scala.collection.JavaConverters;
 import scala.collection.immutable.ArraySeq;
 import scala.collection.immutable.Seq;
 
-record ArrayElement(int index, long value) {}
-
 public class Utils {
+  record ArrayElement(int index, long value) {}
+
   public static ArrayList<Long> convertScalaArrayToArray(ArraySeq<Long> arr) {
     return new ArrayList<Long>(JavaConverters.asJavaCollectionConverter(arr).asJavaCollection());
   }
 
   public static Seq<Long> convertToArrayListToScalaArraySeq(List<Long> arr) {
     return JavaConverters.asScala(arr).iterator().toSeq();
-  }
-
-  public static long t(ArrayElement a, ArrayElement b) {
-    return b.value() - a.value();
   }
 
   public static PriorityQueue<ArrayElement> constructMaxHeap(ArrayList<Long> arr) {
@@ -40,37 +37,13 @@ public class Utils {
     var pq = constructMaxHeap(arr);
     int index = (int) Math.floor(arr.size() * (100 - percent) / 100);
 
-    for (int i = arr.size() - 1; i > index; i--) {
-      pq.poll();
-    }
-
-    return pq.poll().value();
+    return Stream.generate(pq::poll).limit(arr.size() - index).skip(arr.size() - index - 1).findFirst().orElseThrow().value();
   }
 
   public static int quantileIndex(ArrayList<Long> arr, float percent) {
     var pq = constructMaxHeap(arr);
     int index = (int) Math.floor(arr.size() * (100 - percent) / 100);
 
-    for (int i = arr.size() - 1; i > index; i--) {
-      pq.poll();
-    }
-
-    return pq.poll().index();
-  }
-
-  public static int findKthLargestElement(ArrayList<Integer> arr, int k) {
-    if (k < arr.size()) {
-      throw new ArrayIndexOutOfBoundsException();
-    }
-
-    var pq = new PriorityQueue<Integer>(Comparator.reverseOrder());
-
-    pq.addAll(arr);
-
-    for (int i = 0; i < k; i++) {
-      pq.poll();
-    }
-
-    return pq.peek();
+    return Stream.generate(pq::poll).limit(arr.size() - index).skip(arr.size() - index - 1).findFirst().orElseThrow().index();
   }
 }
